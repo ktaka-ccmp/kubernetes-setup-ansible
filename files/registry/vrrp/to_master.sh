006f22c8cb62418faee49b7035b6d343449d26d3
Debian <admin@ip-10-0-10-0.us-west-2.compute.internal>
2016-11-07 11:18:40 +0000

    EIP assignment

--- files/registry/vrrp/to_master.sh
+++ files/registry/vrrp/to_master.sh
@@ -3,11 +3,14 @@
 # VIP kirikae shimasu
 # VIP kaburanai youni chu-i
 
-VIP=10.0.127.251
+ALOCID=eipalloc-f919809e
+VIP=10.0.128.251
 REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone |sed -e 's/[a,c]$//')
 
 mac=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
 ENI=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$mac/interface-id)
 
 aws ec2 assign-private-ip-addresses --network-interface-id $ENI --private-ip-addresses $VIP --allow-reassignment --region $REGION
+aws ec2 associate-address --network-interface-id $ENI --allocation-id $ALOCID --private-ip-address  $VIP --allow-reassociation  --region $REGION
+
 

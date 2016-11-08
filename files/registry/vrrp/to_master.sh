ffc14ea68d801d6f0bfbb3df96f84d429ec6d920
Debian <admin@ip-10-0-10-0.us-west-2.compute.internal>
2016-11-08 06:55:59 +0000

    Variable files

--- files/registry/vrrp/to_master.sh
+++ files/registry/vrrp/to_master.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# VIP kirikae shimasu
-# VIP kaburanai youni chu-i
-
-ALOCID=eipalloc-f919809e
-VIP=10.0.128.251
+ALOCID={{ eipalocid }}
+VIP={{ vip }}
 REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone |sed -e 's/[a-c]$//')
 
 mac=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
@@ -12,5 +9,4 @@ ENI=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$m
 
 aws ec2 assign-private-ip-addresses --network-interface-id $ENI --private-ip-addresses $VIP --allow-reassignment --region $REGION
 aws ec2 associate-address --network-interface-id $ENI --allocation-id $ALOCID --private-ip-address  $VIP --allow-reassociation  --region $REGION
-
 

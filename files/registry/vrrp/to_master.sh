9a1db738f83e21bb59867e87a9301df36c0b3da3
Kimitoshi Takahshi <ktaka@ccmp.jp>
2016-12-15 03:18:34 +0000

    to_master fix

--- files/registry/vrrp/to_master.sh
+++ files/registry/vrrp/to_master.sh
@@ -6,6 +6,10 @@ EIP={{ eip }}
 host=$(curl -s -H "Metadata-Flavor: Google"  http://metadata.google.internal/computeMetadata/v1/instance/hostname | cut -f 1 -d ".")
 zone=$(curl -s -H "Metadata-Flavor: Google"  http://metadata.google.internal/computeMetadata/v1/instance/zone| cut -f 4 -d "/")
 
+gcloud compute instances add-tags $host --zone $zone --tags private
+
 gcloud compute instances delete-access-config $host --zone $zone
 gcloud compute instances add-access-config $host --zone $zone  --address  $EIP
+
+gcloud compute instances remove-tags $host --zone $zone --tags private
 

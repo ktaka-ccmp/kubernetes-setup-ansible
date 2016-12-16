4367eca6929f862179646c10d23f8c87240cb1c8
Kimitoshi Takahshi <ktaka@ccmp.jp>
2016-12-16 09:14:15 +0000

    to_master fix.

--- files/registry/vrrp/to_master.sh
+++ files/registry/vrrp/to_master.sh
@@ -2,14 +2,34 @@
 
 VIP={{ vip }}
 EIP={{ eip }}
+EIP_ID={{ eip_id }}
+
+export https_proxy=http://{{ inventory_hostname }}:8888
+
+#### remove eip from other vm
+uri=$(gcloud compute addresses list --uri|grep $EIP_ID)
+token=$(curl -s  "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" -H "Metadata-Flavor: Google"|jq -r .access_token)
+users=$(curl -s $uri -H "Authorization":"Bearer $token" |jq -r .users[]?)
+
+if [ ! "$users" == "" ] ; then
+        user=$(echo $users|sed -e "s/.*instances\///g")
+        zone=$(echo $users|sed -e "s/.*zones\/\(.*\)\/.*\/.*/\1/g")
+        config=$(curl -s $users -H "Authorization":"Bearer $token" |jq -r .networkInterfaces[].accessConfigs[].name?)
+
+        gcloud compute instances delete-access-config $user --zone $zone --access-config-name "$config" &
+fi
+
+#### assign eip to myself
 
 host=$(curl -s -H "Metadata-Flavor: Google"  http://metadata.google.internal/computeMetadata/v1/instance/hostname | cut -f 1 -d ".")
 zone=$(curl -s -H "Metadata-Flavor: Google"  http://metadata.google.internal/computeMetadata/v1/instance/zone| cut -f 4 -d "/")
+uri=$(gcloud compute instances list --uri|grep $host)
+config=$(curl -s $uri -H "Authorization":"Bearer $token" |jq -r .networkInterfaces[]?.accessConfigs[]?.name?)
 
-gcloud compute instances add-tags $host --zone $zone --tags private
+if [  "$config" != "" -a "$host" != "$user" ] ; then
+        gcloud compute instances delete-access-config $host --zone $zone --access-config-name "$config" &
+fi
 
-gcloud compute instances delete-access-config $host --zone $zone
+wait
 gcloud compute instances add-access-config $host --zone $zone  --address  $EIP
-
-gcloud compute instances remove-tags $host --zone $zone --tags private
 

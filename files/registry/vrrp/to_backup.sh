e82bbc4974478dac9c5fe6471439d70214c7a4ff
Kimitoshi Takahshi <ktaka@ccmp.jp>
2017-03-17 01:44:05 +0900

    ip netns for vrrp, thus keepalived will run default namespace

--- files/registry/vrrp/to_backup.sh
+++ files/registry/vrrp/to_backup.sh
@@ -2,6 +2,7 @@
 
 [[ -f /var/run/netns/pod ]] || ln -s /proc/1/ns/net /var/run/netns/pod
 
-ip netns exec pod ip add add {{ vip }}/32 dev eth0
-arping -c 5 -i eth0 -PU {{ vip }}
+ip netns exec pod ip add del {{ vip }}/32 dev eth0
+
+
 

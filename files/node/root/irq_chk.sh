57fed085f14a42f437ccb9a54e76de19a0813004
Kimitoshi Takahshi <ktaka@ccmp.jp>
2018-10-05 03:29:23 +0900

    Settings for benchmarks ... wip

---
+++ files/node/root/irq_chk.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+IRQS=$(cat /proc/interrupts |egrep "eth0-tx"|cut -f 1 -d :|tr -d '\n')
+
+for irq in $IRQS ; do
+        file=/proc/irq/$irq/smp_affinity
+        echo -n $file ": "
+	cat $file
+done
+
+for file in /sys/class/net/eth0/queues/tx-*/xps_cpus ; do
+        echo -n $file ": "
+	cat $file
+done
+
+
+IRQS=$(cat /proc/interrupts |egrep "eth0-rx"|cut -f 1 -d :|tr -d '\n')
+
+for irq in $IRQS ; do
+        file=/proc/irq/$irq/smp_affinity
+        echo -n $file ": "
+	cat $file
+done
+
+for file in /sys/class/net/eth0/queues/rx-*/rps_cpus ; do
+        echo -n $file ": "
+	cat $file
+done
+
+file=/proc/sys/net/core/rps_sock_flow_entries
+        echo -n $file ": "
+	cat $file
+
+for file in /sys/class/net/eth0/queues/rx-*/rps_flow_cnt ; do
+        echo -n $file ": "
+	cat $file
+done
+
